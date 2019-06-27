a97e277b8d91075af7b9c3a0ec39cfbf6cf17ca4
wkvanderveen <w.k.van.der.veen.2@student.rug.nl>
2019-06-27 16:42:20 +0200

    Remove unused code in local gan

--- augment.py
+++ augment.py
@@ -5,145 +5,179 @@ from os import listdir, mkdir
 from os.path import isfile, isdir, join, abspath
 from copy import deepcopy
 
-ROBINPATH 	= abspath("../ROBIN")
+ROBINPATH = abspath("../ROBIN")
 COMPLEXPATH = abspath("../Dataset_complex")
-OUTPATH 	= abspath("./augmented")
+OUTPATH = abspath("./augmented")
 RESIZE_FACTOR = 32
 
-def padd_h(image, padding_height):
-	m = np.array([255], dtype=np.uint8)
-	(_, maxx) = image.shape
-	padding_top = matlib.repmat(m, padding_height // 2, maxx)
-	padding_bot = matlib.repmat(m, padding_height // 2 + padding_height % 2, maxx) #take care of the floored pixel
 
-	return np.concatenate((padding_top, image, padding_bot), axis=0)
+def padd_h(image, padding_height):
+    m = np.array([255], dtype=np.uint8)
+    (_, maxx) = image.shape
+    padding_top = matlib.repmat(m, padding_height // 2, maxx)
+    padding_bot = matlib.repmat(m,
+                                padding_height // 2 + padding_height % 2,
+                                maxx)  # take care of the floored pixel
+
+    return np.concatenate((padding_top, image, padding_bot), axis=0)
+
 
 def padd_v(image, padding_width):
-	m = np.array([255], dtype=np.uint8)
-	(maxy, _) = image.shape
-	padding_left = matlib.repmat(m, maxy, padding_width // 2)
-	padding_right = matlib.repmat(m, maxy, padding_width // 2 + padding_width % 2)
+    m = np.array([255], dtype=np.uint8)
+    (maxy, _) = image.shape
+    padding_left = matlib.repmat(m, maxy, padding_width // 2)
+    padding_right = matlib.repmat(m,
+                                  maxy,
+                                  padding_width // 2 + padding_width % 2)
 
-	return np.concatenate((padding_left, image, padding_right), axis=1)
+    return np.concatenate((padding_left, image, padding_right), axis=1)
+
 
 def padd_to_biggest_file(image, maxh, maxw):
-	(maxy, maxx) = image.shape
-	img = padd_h(image, maxh - maxy)
-	return padd_v(img, maxw - maxx)
+    (maxy, maxx) = image.shape
+    img = padd_h(image, maxh - maxy)
+    return padd_v(img, maxw - maxx)
+
 
 def scale_and_padd_to_biggest_file(image, maxh, maxw):
-	## todo implement scaling
-	(maxy, maxx) = image.shape
-	scale = maxw / maxx if maxh / maxy > maxw / maxx else maxh / maxy #take smallest scale to not go out of bounds
+    # todo implement scaling
+    (maxy, maxx) = image.shape
+    # take smallest scale to not go out of bounds
+    scale = maxw / maxx if maxh / maxy > maxw / maxx else maxh / maxy
 
-	image = cv2.resize(image, (int(maxx * scale),int(maxy * scale) ) )
+    image = cv2.resize(image, (int(maxx * scale), int(maxy * scale)))
 
-	return padd_to_biggest_file(image, maxh, maxw)
+    return padd_to_biggest_file(image, maxh, maxw)
 
 
 def loadAllFiles(path):
-    files =  [join(path, f) for f in listdir(path) if isfile(join(path, f))]
+    files = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
     folders = [join(path, f) for f in listdir(path) if isdir(join(path, f))]
     for folder in folders:
-    	temp =  [join(folder, f) for f in listdir(folder) if isfile(join(folder, f))]
-    	for f in temp:
-    		files.append(f) #slow and ugly, fix later
+        temp = [join(folder, f) for f in listdir(folder)
+                if isfile(join(folder, f))]
+        for f in temp:
+            files.append(f)  # slow and ugly, fix later
 
     print("found %d files on %s" % (len(files), str(path)))
     return files
 
+
 def rotate_image(img):
-	(maxy, maxx) = np.shape(img)
-	rotated = np.zeros((maxx, maxy), dtype=np.uint8)
+    (maxy, maxx) = np.shape(img)
+    rotated = np.zeros((maxx, maxy), dtype=np.uint8)
 
-	for y in range(maxy):
-		for x in range(maxx):
-			rotated[x, y] = img[y, x]
+    for y in range(maxy):
+        for x in range(maxx):
+            rotated[x, y] = img[y, x]
 
-	return rotated
-
-def augment_images(images, filename, flip = True, saveiter = 1000000, saveaspng=False, resize_height=0, resize_width=0, max_height = 0, max_width = 0):
-	'''
-	Flip should be set to false for the advanced dataset
-	saveiter is the iteration at which the images get saved (and the ram freed)
-
-	Rotation code is now very ugly, but this is due to the rotation code being very inefficient, so I tried to use
-	it as little as possible
-	'''
-	final_images = []
-	saveidx = 0
-
-	for idx, imgpath in enumerate(images):
-		original_img = cv2.imread(imgpath)
-		original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY) #set to one channel
-		original_img = scale_and_padd_to_biggest_file(original_img, max_height, max_width)
-		original_img = cv2.resize(original_img, (resize_width, resize_height))
-
-		final_images.append(deepcopy(original_img))
-
-		#add rotations
-		img = deepcopy(original_img)
-		final_images.append(np.flip(img, 0))
-		rot_img = rotate_image(img)
-		final_images.append(rot_img)
-		final_images.append(np.flip(rot_img, 1))
-
-		#mirror and rotate
-		if flip:
-			img = np.flip(original_img, 1);
-			final_images.append(deepcopy(img))
-			final_images.append(np.flip(img, 0))
-			rot_img = rotate_image(img)
-			final_images.append(rot_img)
-			final_images.append(np.flip(rot_img, 1))
-
-		#save images to numpy array
-		if idx % saveiter == 0 and not saveaspng: #Don't run this part, we want to save in 1 file
-			np.save(join(OUTPATH, '%s_%d' % (filename, saveidx)), final_images)
-			saveidx += 1
-			final_images = []
-
-		#save all images individually
-		if saveaspng:
-			for img in final_images:
-				cv2.imwrite('filename_%d.png' % (saveidx), img)
-				saveidx += 1
-			final_images = []
+    return rotated
 
 
-	np.save(join(OUTPATH, '%s_%d' % (filename, saveidx)), final_images)
+def augment_images(images, filename, flip=True, saveiter=1000000,
+                   saveaspng=False, resize_height=0, resize_width=0,
+                   max_height=0, max_width=0):
+    '''
+    Flip should be set to false for the advanced dataset
+    saveiter is the iteration at which the images get saved (and the ram freed)
 
-	print("augmented and saved all files")
+    Rotation code is now very ugly, but this is due to the rotation code being
+    very inefficient, so I tried to use it as little as possible
+    '''
+    final_images = []
+    saveidx = 0
+
+    for idx, imgpath in enumerate(images):
+        original_img = cv2.imread(imgpath)
+
+        # set to one channel
+        original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
+        original_img = scale_and_padd_to_biggest_file(original_img,
+                                                      max_height,
+                                                      max_width)
+        original_img = cv2.resize(original_img, (resize_width, resize_height))
+
+        final_images.append(deepcopy(original_img))
+
+        # add rotations
+        img = deepcopy(original_img)
+        final_images.append(np.flip(img, 0))
+        rot_img = rotate_image(img)
+        final_images.append(rot_img)
+        final_images.append(np.flip(rot_img, 1))
+
+        # mirror and rotate
+        if flip:
+            img = np.flip(original_img, 1)
+            final_images.append(deepcopy(img))
+            final_images.append(np.flip(img, 0))
+            rot_img = rotate_image(img)
+            final_images.append(rot_img)
+            final_images.append(np.flip(rot_img, 1))
+
+        # save images to numpy array
+        # Don't run this part, we want to save in 1 file
+        if idx % saveiter == 0 and not saveaspng:
+            np.save(join(OUTPATH, '%s_%d' % (filename, saveidx)), final_images)
+            saveidx += 1
+            final_images = []
+
+        # save all images individually
+        if saveaspng:
+            for img in final_images:
+                cv2.imwrite('filename_%d.png' % (saveidx), img)
+                saveidx += 1
+            final_images = []
+
+    np.save(join(OUTPATH, '%s_%d' % (filename, saveidx)), final_images)
+
+    print("augmented and saved all files")
+
 
 def get_max_dims(images):
-	# Get size of largest image
-	max_height = 0
-	max_width = 0
+    # Get size of largest image
+    max_height = 0
+    max_width = 0
 
-	for idx, imgpath in enumerate(images):
-		original_img = cv2.imread(imgpath)
-		(height, width, _) = original_img.shape
-		if height > max_height:
-			max_height = height
-		if width > max_width:
-			max_width = width
-	resize_shape_height = max_height//RESIZE_FACTOR
-	resize_shape_width = max_width//RESIZE_FACTOR
+    for idx, imgpath in enumerate(images):
+        original_img = cv2.imread(imgpath)
+        (height, width, _) = original_img.shape
+        if height > max_height:
+            max_height = height
+        if width > max_width:
+            max_width = width
+    resize_shape_height = max_height//RESIZE_FACTOR
+    resize_shape_width = max_width//RESIZE_FACTOR
 
-	return (resize_shape_height, resize_shape_width, max_height, max_width)
+    return (resize_shape_height, resize_shape_width, max_height, max_width)
 
 
 if __name__ == '__main__':
 
-	files_robin = loadAllFiles(ROBINPATH)
-	files_complex = loadAllFiles(COMPLEXPATH)
-	(height, width, max_height, max_width) = get_max_dims(files_robin + files_complex)
+    files_robin = loadAllFiles(ROBINPATH)
+    files_complex = loadAllFiles(COMPLEXPATH)
+    (height, width, max_height, max_width) = get_max_dims(
+        files_robin + files_complex)
 
-	if not isdir(OUTPATH):
-		mkdir(OUTPATH)
+    if not isdir(OUTPATH):
+        mkdir(OUTPATH)
 
-	augment_images(files_robin, 'robin_data', resize_height=height, resize_width=width, max_height = max_height, max_width=max_width)
-	print("Done with ROBIN")
-	augment_images(files_complex, 'complex_data', resize_height=height, resize_width=width, max_height = max_height, max_width=max_width)
-	print("Done with Complex")
-	print("FINISHED")
+    augment_images(files_robin,
+                   'robin_data',
+                   resize_height=height,
+                   resize_width=width,
+                   max_height=max_height,
+                   max_width=max_width)
+
+    print("Done with ROBIN")
+
+    augment_images(files_complex,
+                   'complex_data',
+                   resize_height=height,
+                   resize_width=width,
+                   max_height=max_height,
+                   max_width=max_width)
+
+    print("Done with Complex")
+
+    print("FINISHED")
--- gan.py
+++ gan.py
@@ -1,26 +1,13 @@
-#@title  { form-width: "250px" }
 from __future__ import print_function, division
-from keras.datasets import mnist
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout, Conv2D
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
-from keras.layers import Conv2DTranspose, MaxPooling2D, Concatenate, LeakyReLU
-from keras.layers import Dropout
-from keras.layers.convolutional import UpSampling2D, Conv2D
-from keras.models import Sequential, Model, load_model
+from keras.layers import Input, Dense, Reshape, Flatten, Conv2D
+from keras.layers import BatchNormalization
+from keras.layers import MaxPooling2D, Concatenate, LeakyReLU
+from keras.models import Model
 from keras.optimizers import Adam
-from keras.preprocessing.image import load_img, img_to_array, array_to_img
 from keras.callbacks import TensorBoard
-from PIL import Image
-import PIL.ImageOps
 import matplotlib.pyplot as plt
 import os
 import shutil
-from random import choice, randint
-import glob, os
-from PIL import Image
-import PIL.ImageOps
-import matplotlib.pyplot as plt
-import sys
 import numpy as np
 
 PREPARE_COLAB_DATA = False
@@ -29,22 +16,20 @@ NPY_SAVEFILE = 'traindata.npy'
 IMAGE_DIR = 'images/'
 LOG_DIR = './logs'
 TRAIN_ON_AUGMENTED = True
-SIMPLE_DATA = ['./robin_data_0.npy']
-COMPLEX_DATA = ['./complex_data_0.npy']
-#ALL_NPYS = os.listdir('./augmented')
+SIMPLE_DATA = ['./augmented/robin_data_0.npy']
+COMPLEX_DATA = ['./augmented/complex_data_0.npy']
 
 EPOCHS = 30000
 BATCH_SIZE = 16
 SAMPLE_INTERVAL = 100
 RESCALE_FACTOR = 32
-# TRAIN_ON_COMPLEX = False
+
 
 class GAN():
     def __init__(self):
         self.channels = 1
         self.latent_dim = 50
 
-        #self.img_size = np.load('./augmented/'+ALL_NPYS[0], allow_pickle=True)[0].shape
         self.img_size = np.load(SIMPLE_DATA[0])[0].shape
         self.img_size += (1,)  # add color channel for conv layers
 
@@ -74,35 +59,18 @@ class GAN():
                 try:
                     if os.path.isfile(file_path):
                         os.unlink(file_path)
-                    elif os.path.isdir(file_path): shutil.rmtree(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
                 except Exception as e:
                     print(e)
-        except:
+        except FileExistsError:
             print("./images dir does not yet exist")
-
-        # Load the dataset
-        filelist = glob.glob("./source_imgs/*.jpg")
-        imgs = [Image.open(fname) for fname in filelist]
-        # if RUN_ON_COLAB:
-        #     try:
-        #         os.mkdir(IMAGE_DIR)
-        #         print("Created output images directory...")
-        #     except:
-        #         print("Output images directory already exists!")
-        #     if TRAIN_ON_AUGMENTED:
-        #         self.X_train = np.load(COMPLEX_DATA[0], allow_pickle=True)
-        #     else:
-        #         self.X_train = np.stack(np.load(NPY_SAVEFILE, allow_pickle=True))
-        #     self.X_train = np.expand_dims(self.X_train, axis=3)
-        #     target_size = (max([x.shape[1] for x in self.X_train]), max([x.shape[0] for x in self.X_train]))
-        #     #target_size = (self.X_train[0].shape[1], self.X_train[1].shape[0])
-        #     self.img_shape = (target_size[1], target_size[0], self.channels)
 
         # Build and compile the discriminator
         self.discriminator = self.build_discriminator()
         self.discriminator.compile(loss='binary_crossentropy',
-            optimizer=optimizer,
-            metrics=['accuracy'])
+                                   optimizer=optimizer,
+                                   metrics=['accuracy'])
 
         # Build the generator
         self.generator = self.build_generator()
@@ -114,14 +82,13 @@ class GAN():
         # For the combined model we will only train the generator
         self.discriminator.trainable = False
 
-        # The discriminator takes generated images as input and determines validity
+        # Discriminator takes generated images as input and determines validity
         validity = self.discriminator(img)
 
-        # The combined model  (stacked generator and discriminator)
+        # The combined model (stacked generator and discriminator)
         # Trains the generator to fool the discriminator
         self.combined = Model(z, validity)
         self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
-
 
     def build_generator(self):
 
@@ -131,18 +98,10 @@ class GAN():
                        input_shape=(self.latent_dim,))(inp)
         layer1 = LeakyReLU()(layer1)
         layer1 = BatchNormalization(momentum=0.8)(layer1)
-        # layer1 = Dropout(rate=0.5)(layer1)
 
         layer2 = Dense(256)(layer1)
         layer2 = LeakyReLU()(layer2)
         layer2 = BatchNormalization(momentum=0.8)(layer2)
-
-        # layer2 = Dropout(rate=0.5)(layer2)
-
-        # layer3 = Dense(128)(layer2)
-        # layer3 = LeakyReLU()(layer3)
-        # layer3 = BatchNormalization(momentum=0.8)(layer3)
-        # layer3 = Dropout(rate=0.5)(layer3)
 
         concat = Concatenate(axis=-1)([layer1, layer2])
 
@@ -176,7 +135,6 @@ class GAN():
         flat_conv2 = Flatten()(conv2)
         flat_conv2 = Dense(56, activation='relu')(flat_conv2)
 
-
         fc = Concatenate()([flat_conv1, flat_conv2])
 
         fc = Dense(128, activation='relu')(fc)
@@ -200,21 +158,17 @@ class GAN():
             # ---------------------
 
             # Select a random batch of images
-            #this_npy = randint(0, len(ALL_NPYS)-1)
-            #this_npy_num_imgs = np.load('./augmented/'+ALL_NPYS[this_npy], allow_pickle=True).shape[0]
-            this_npy_num_imgs = np.load(SIMPLE_DATA[0], allow_pickle=True).shape[0]
+            this_npy_num_imgs = np.load(SIMPLE_DATA[0],
+                                        allow_pickle=True)
+            this_npy_num_imgs = this_npy_num_imgs.shape[0]
             batch_size = min(this_npy_num_imgs, batch_size)
-
 
             # Adversarial ground truths
             valid = np.ones((batch_size, 1))
             fake = np.zeros((batch_size, 1))
 
-
             idx = np.random.randint(0, this_npy_num_imgs-1, batch_size)
 
-
-            #self.X_train = np.load('./augmented/'+ALL_NPYS[this_npy], allow_pickle=True)[idx]
             self.X_train = np.load(SIMPLE_DATA[0], allow_pickle=True)[idx]
             self.X_train = np.expand_dims(self.X_train, axis=3)
             self.X_train = self.X_train / (255/2) - 1
@@ -226,12 +180,14 @@ class GAN():
 
             if epoch == 0 or accuracy < 80:
                 # Train the discriminator
-                d_loss_real = self.discriminator.train_on_batch(self.X_train, valid)
+                d_loss_real = self.discriminator.train_on_batch(self.X_train,
+                                                                valid)
                 d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
                 d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
             else:
                 # Test the discriminator
-                d_loss_real = self.discriminator.test_on_batch(self.X_train, valid)
+                d_loss_real = self.discriminator.test_on_batch(self.X_train,
+                                                               valid)
                 d_loss_fake = self.discriminator.test_on_batch(gen_imgs, fake)
                 d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
@@ -243,26 +199,22 @@ class GAN():
             noise = np.random.normal(-1, 1, (batch_size, self.latent_dim))
 
             if epoch == 0 or accuracy > 20:
-                # Train the generator (to have the discriminator label samples as valid)
+                # Train the generator (to have the discriminator label samples
+                # as valid)
                 g_loss = self.combined.train_on_batch(noise, valid)
             else:
-                # Train the generator (to have the discriminator label samples as valid)
+                # Train the generator (to have the discriminator label samples
+                # as valid)
                 g_loss = self.combined.test_on_batch(noise, valid)
 
-            tensorboard.on_epoch_end(epoch, {'generator loss': g_loss, 'discriminator loss': d_loss[0], 'Accuracy': accuracy})
-
-            # Plot the progress
-            if RUN_ON_COLAB:
-                if (epoch % 10) == 0:
-                    print(f"{epoch} [D loss: {d_loss[0]}, " +
-                  f"acc.: {accuracy}%] [G loss: {g_loss}]")
-            else:
-                print(f"{epoch} [D loss: {d_loss[0]:.3f}, " +
-                  f"acc.: {accuracy:.2f}%] [G loss: {g_loss:.3f}]")
+            tensorboard.on_epoch_end(epoch, {'generator loss': g_loss,
+                                             'discriminator loss': d_loss[0],
+                                             'Accuracy': accuracy})
 
             # If at save interval => save generated image samples
             if epoch % sample_interval == 0:
                 self.sample_images(epoch)
+
         tensorboard.on_train_end()
         self.discriminator.save('discriminator.h5')
         self.generator.save('generator.h5')
@@ -272,7 +224,8 @@ class GAN():
         noise = np.random.normal(-1, 1, (r, self.latent_dim))
         gen_imgs = self.generator.predict(noise)
         # Rescale images from [-1, 1] to [1, 0] (invert)
-        real_imgs = self.X_train[np.random.choice(self.X_train.shape[0]), :, :, 0]
+        real_imgs = self.X_train[np.random.choice(
+            self.X_train.shape[0]), :, :, 0]
         # gen_imgs = -0.5 * gen_imgs - 0.5
         # real_imgs = -0.5 * real_imgs - 0.5
 
@@ -283,9 +236,9 @@ class GAN():
 
         cnt = 0
         for i in range(r-1):
-                axs[i+1].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
-                axs[i+1].axis('off')
-                cnt += 1
+            axs[i+1].imshow(gen_imgs[cnt, :, :, 0], cmap='gray')
+            axs[i+1].axis('off')
+            cnt += 1
 
         fig.savefig(IMAGE_DIR+"%d.png" % epoch)
         plt.close()
